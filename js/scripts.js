7932786d8495971a01ab8ce52fbb163da9a2f644
sftengahv! <armandohernandezv@icloud.com>
2025-10-15 21:06:50 -0400

    software engineer immersion exercise 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
-alert('Hello World');
-
-let myName= 'Bob';
-document.write(myName);
-
-myName = ('John Doe');
-document.write('myName');
-
-let simpleAddition = 5 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize= size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+let pokemonlist = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poision']
+    },
+    {
+        name: 'CHarmander',
+        height: 6,
+        types: ['fire']
+    },
+    {
+        name: 'Squirtle',
+        height: 5,
+        types: ['water']
+    }
+];
