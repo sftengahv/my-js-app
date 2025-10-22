b6e576ca67ec5e76d9b6b4664e2e966443433a88
sftengahv! <armandohernandezv@icloud.com>
2025-10-21 23:22:54 -0400

    exercise 4

---
+++ calculatortaskpart1/README.md
@@ -0,0 +1,6 @@
+#  CalculatorTaskPart1
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/armandohern8/pen/wBMmKVy](https://codepen.io/armandohern8/pen/wBMmKVy).
+
---
+++ calculatortaskpart1/dist/script.js
@@ -0,0 +1,19 @@
+Funtion add (num1, num2){
+  return num1 + num2;
+}
+
+function subract (num1, num2){
+  return num1 - num2;
+}
+
+function multiply (num1, numb2){
+  return num1 * numb2;
+}
+
+function divide (num1, num2){
+  if(num2 === 0){ 
+    return "NOT ALLOWED";
+} else {
+  return num1/num2;
+}
+}
